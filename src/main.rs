93c3a1fced480e32064ca98cc3dc6f2e23cb95fa
Aaron Weiss <aaronweiss74@gmail.com>
2014-09-24 16:54:55 -0400

    Moved bot functionality from library-side to application-side.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 extern crate irc;
 
+use irc::data::Message;
+
 fn main() {
-    let mut pickle = irc::Bot::new().unwrap();
+    let process = |bot: &irc::Bot, source: &str, command: &str, args: &[&str]| {
+        match (command, args) {
+            ("PRIVMSG", [chan, msg]) => {
+                if msg.contains("pickles") && msg.contains("hi") {
+                    try!(irc::conn::send(&bot.conn, Message::new(None, "PRIVMSG", [chan.as_slice(), "hi"])));
+                } else if msg.starts_with(". ") {
+                    try!(irc::conn::send(&bot.conn, Message::new(None, "PRIVMSG", [chan.as_slice(), msg.slice_from(2)])));
+                };
+            },
+            _ => ()
+        }
+        Ok(())
+    };
+    let mut pickle = irc::Bot::new(process).unwrap();
     pickle.identify().unwrap();
     pickle.output();
 }
