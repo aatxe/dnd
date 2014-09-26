f1bc532bd82e7d8b87f2a4028723de5e9a36f622
Aaron Weiss <aaronweiss74@gmail.com>
2014-09-26 00:31:36 -0400

    Implemented account creation over IRC.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 extern crate irc;
 extern crate serialize;
 
+use data::Player;
+
 mod data;
+
+fn str_to_u8(s: &str) -> u8 {
+    match from_str(s) {
+        Some(n) => n,
+        None => 0,
+    }
+}
 
 #[cfg(not(test))]
 fn main() {
@@ -16,27 +25,51 @@ fn main() {
                         None => chan,
                     }
                 };
-                if msg.contains("pickles") && msg.contains("hi") {
-                    try!(bot.send_privmsg(resp, "hi"))
-                } else if msg.starts_with(". ") {
-                    try!(bot.send_privmsg(resp, msg.slice_from(2)));
-                } else if msg.starts_with(".list") {
-                    let mut s = String::new();
-                    match bot.chanlists.find(&String::from_str(chan)) {
-                        Some(vec) => {
-                            for user in vec.iter() {
-                                s.push_str(user.as_slice());
-                                s.push_char(' ');
+                if !chan.starts_with("#") {
+                    if msg.starts_with("create") {
+                        let params: Vec<&str> = msg.split_str(" ").collect();
+                        if params.len() == 9 {
+                            let mut valid = true;
+                            for s in params.slice_from(3).iter() {
+                                if str_to_u8(*s) == 0 {
+                                    valid = false;
+                                }
                             }
-                            let len = s.len() - 1;
-                            s.truncate(len);
-                        },
-                        None => {
-                            s.push_str("None.");
+                            if valid {
+                                let p = try!(Player::create(params[1], params[2],
+                                    str_to_u8(params[3]), str_to_u8(params[4]),
+                                    str_to_u8(params[5]), str_to_u8(params[6]),
+                                    str_to_u8(params[7]), str_to_u8(params[8])));
+                                try!(p.save());
+                                try!(bot.send_privmsg(resp, "Your account has been created."));
+                            } else {
+                                try!(bot.send_privmsg(resp, "Stats must be non-zero positive integers. Format is: "))
+                                try!(bot.send_privmsg(resp, "create username password str dex con wis int cha"));
+                            }
+                        } else {
+                            try!(bot.send_privmsg(resp, "Incorrect format for creation. Format is:"));
+                            try!(bot.send_privmsg(resp, "create username password str dex con wis int cha"));
                         }
                     }
-                    try!(bot.send_privmsg(resp, s.as_slice()));
-                };
+                } else {
+                    if msg.starts_with(".list") {
+                        let mut s = String::new();
+                        match bot.chanlists.find(&String::from_str(chan)) {
+                            Some(vec) => {
+                                for user in vec.iter() {
+                                    s.push_str(user.as_slice());
+                                    s.push_char(' ');
+                                }
+                                let len = s.len() - 1;
+                                s.truncate(len);
+                            },
+                            None => {
+                                s.push_str("None.");
+                            }
+                        }
+                        try!(bot.send_privmsg(resp, s.as_slice()));
+                    }
+                }
             },
             _ => (),
         }
