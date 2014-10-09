3d790bc03cfccc45bbda44da2bc37a007044746e
Aaron Weiss <aaronweiss74@gmail.com>
2014-10-09 15:54:07 -0400

    Moved process closure into a function, process_world, that takes a world to operate on.

--- src/func/mod.rs
+++ src/func/mod.rs
@@ -4,11 +4,58 @@ extern crate irc;
 use std::io::IoResult;
 use data::world::World;
 use irc::Bot;
+use irc::bot::IrcBot;
+use irc::data::{IrcReader, IrcWriter};
 
 pub mod entity;
 pub mod monster;
 pub mod player;
 pub mod world;
+
+pub fn process_world<T, U>(bot: &IrcBot<T, U>, source: &str, command: &str, args: &[&str], world: &mut World) -> IoResult<()> where T: IrcWriter, U: IrcReader {
+    match (command, args) {
+        ("PRIVMSG", [chan, msg]) => {
+            let user = match source.find('!') {
+                Some(i) => source.slice_to(i),
+                None => chan,
+            };
+            let tokens: Vec<&str> = msg.split_str(" ").collect();
+            try!(if !chan.starts_with("#") {
+                match tokens[0] {
+                    "register" => player::register(bot, user, tokens),
+                    "login" => player::login(bot, user, world, tokens),
+                    "create" => world::create(bot, user, world, tokens),
+                    "logout" => player::logout(bot, user, world),
+                    "addfeat" => player::add_feat(bot, user, world, tokens),
+                    "roll" => world::private_roll(bot, user),
+                    "saveall" => world::save_all(bot, user, world),
+                    "save" => player::save(bot, user, world),
+                    "lookup" => player::look_up(bot, user, world, tokens),
+                    "mlookup" => monster::look_up(bot, user, world, tokens),
+                    "addmonster" => monster::add(bot, user, world, tokens),
+                    _ => Ok(())
+                }
+            } else {
+                if tokens[0].starts_with(".") {
+                    match tokens[0].slice_from(1) {
+                        "roll" => entity::roll(bot, user, chan, world, tokens),
+                        "lookup" => player::look_up(bot, user, world, tokens),
+                        "update" => player::add_update(bot, user, chan, world, tokens, true),
+                        "increase" => player::add_update(bot, user, chan, world, tokens, false),
+                        "temp" => entity::set_temp_stats(bot, user, chan, world, tokens),
+                        "cleartemp" => entity::clear_temp_stats(bot, user, chan, world, tokens),
+                        "damage" => entity::damage(bot, user, chan, world, tokens),
+                        _ => Ok(())
+                    }
+                } else {
+                    Ok(())
+                }
+            });
+        },
+        _ => (),
+    }
+    Ok(())
+}
 
 pub fn permissions_test(bot: &Bot, user: &str, chan: &str, world: &mut World) -> IoResult<bool> {
     let mut ret = true;
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ extern crate crypto;
 extern crate irc;
 extern crate serialize;
 
-use std::io::{BufferedReader, BufferedWriter, TcpStream};
 use data::world::World;
-use irc::{Bot};
+use irc::Bot;
 use irc::bot::IrcBot;
 
 mod data;
@@ -14,51 +13,9 @@ mod func;
 #[cfg(not(test))]
 fn main() {
     let mut world = World::new().unwrap();
-    let process = |bot: &IrcBot<BufferedWriter<TcpStream>, BufferedReader<TcpStream>>, source: &str, command: &str, args: &[&str]| {
-        match (command, args) {
-            ("PRIVMSG", [chan, msg]) => {
-                let user = match source.find('!') {
-                    Some(i) => source.slice_to(i),
-                    None => chan,
-                };
-                let tokens: Vec<&str> = msg.split_str(" ").collect();
-                try!(if !chan.starts_with("#") {
-                    match tokens[0] {
-                        "register" => func::player::register(bot, user, tokens),
-                        "login" => func::player::login(bot, user, &mut world, tokens),
-                        "create" => func::world::create(bot, user, &mut world, tokens),
-                        "logout" => func::player::logout(bot, user, &mut world),
-                        "addfeat" => func::player::add_feat(bot, user, &mut world, tokens),
-                        "roll" => func::world::private_roll(bot, user),
-                        "saveall" => func::world::save_all(bot, user, &world),
-                        "save" => func::player::save(bot, user, &mut world),
-                        "lookup" => func::player::look_up(bot, user, &mut world, tokens),
-                        "mlookup" => func::monster::look_up(bot, user, &mut world, tokens),
-                        "addmonster" => func::monster::add(bot, user, &mut world, tokens),
-                        _ => Ok(())
-                    }
-                } else {
-                    if tokens[0].starts_with(".") {
-                        match tokens[0].slice_from(1) {
-                            "roll" => func::entity::roll(bot, user, chan, &mut world, tokens),
-                            "lookup" => func::player::look_up(bot, user, &mut world, tokens),
-                            "update" => func::player::add_update(bot, user, chan, &mut world, tokens, true),
-                            "increase" => func::player::add_update(bot, user, chan, &mut world, tokens, false),
-                            "temp" => func::entity::set_temp_stats(bot, user, chan, &mut world, tokens),
-                            "cleartemp" => func::entity::clear_temp_stats(bot, user, chan, &mut world, tokens),
-                            "damage" => func::entity::damage(bot, user, chan, &mut world, tokens),
-                            _ => Ok(())
-                        }
-                    } else {
-                        Ok(())
-                    }
-                });
-            },
-            _ => (),
-        }
-        Ok(())
-    };
-    let mut pickle = IrcBot::new(process).unwrap();
-    pickle.identify().unwrap();
-    pickle.output().unwrap();
+    let mut bot = IrcBot::new(|bot, source, command, args| {
+        func::process_world(bot, source, command, args, &mut world)
+    }).unwrap();
+    bot.identify().unwrap();
+    bot.output().unwrap();
 }
